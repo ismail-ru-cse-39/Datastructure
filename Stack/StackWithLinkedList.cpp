9dcead46da09d0ad36b3464ac642c60e202e53dc
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2020-06-02 17:07:12 +0000

    Stacke is implemented using link list.

--- Stack/StackWithLinkedList.cpp
+++ Stack/StackWithLinkedList.cpp
@@ -42,12 +42,67 @@ int top()
     return Start->val;
 }
 
+void pop()
+{
+    if(Start == NULL) {
+        printf("Stack is empty.\n");
+        return;
+    }
+
+    Start = Start->next;
+}
+
+void printStack()
+{
+    struct Node *node = Start;
+
+    if(Start == NULL) {
+        printf("Stack is empty.\n");
+        return;
+    }
+    while(node != NULL) {
+        printf("%d ", node->val);
+        node = node->next;
+    }
+
+    printf("\n");
+}
+
 int main()
 {
     init();
+    while(true) {
+        int choice;
+        printf("Press 1 to push, 2 to see top 3 to pop and 4 to print the Stack 5 to exit\n");
+        scanf("%d", &choice);
 
-    int val;
-    scanf("%d", &val);
-    push(val);
-    int value = top();
+        if(choice == 1) {
+            int val;
+            printf("Enter the val.\n");
+            scanf("%d", &val);
+            push(val);
+        }
+
+        if(choice == 2) {
+            int topVal = top();
+            printf("Current top value : %d\n", topVal);
+        }
+
+        if(choice == 3) {
+            pop();
+        }
+
+        if(choice == 4){
+            printf("All elements of stack: \n");
+            printStack();
+        }
+
+        if(choice == 5) {
+            return 0;
+        }
+
+    }
+
+
+    return 0;
 }
